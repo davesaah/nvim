01f1dc835a4e82b1bb7ed4442b964730c2a8cc18
David Saah <dave@davesaah.com>
2025-11-22 09:14:56 +0000

    remove flutter and haskell plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,21 +22,21 @@ return {
     event = 'VeryLazy',
   },
 
-  -- flutter
-  {
-    'nvim-flutter/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-    config = true,
-  },
-
-  -- haskell
-  {
-    'mrcjkb/haskell-tools.nvim',
-    version = '^4', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
+  -- -- flutter
+  -- {
+  --   'nvim-flutter/flutter-tools.nvim',
+  --   lazy = false,
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'stevearc/dressing.nvim', -- optional for vim.ui.select
+  --   },
+  --   config = true,
+  -- },
+  --
+  -- -- haskell
+  -- {
+  --   'mrcjkb/haskell-tools.nvim',
+  --   version = '^4', -- Recommended
+  --   lazy = false, -- This plugin is already lazy
+  -- },
 }
